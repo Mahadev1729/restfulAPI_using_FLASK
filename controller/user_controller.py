8910956b84b1b2325610aa141dacf11f1e17b35d
Mahadev1729 <devcampus17@gmail.com>
2025-08-31 22:36:51 +0530

    Implement user signup functionality and clean up imports

--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,4 +1,6 @@
 ﻿from app import app
+from model.user_model import user_model
+obj=user_model()
 @app.route("/user/signup")
-def signup():
-    return "This is signup"
+def user_signup_controller():
+    return obj.user_signup_model()
